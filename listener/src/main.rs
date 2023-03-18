8d11754a933e832d0c339102cff7235b0d5c1e30
AVONSTU1 <AVONSTU1@ford.com>
2023-03-17 22:24:25 -0400

    init

---
+++ listener/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::{Read};
+use std::net::{TcpListener, TcpStream};
+
+fn main() {
+    let address = "127.0.0.1:7878";
+    let listener = TcpListener::bind(address).unwrap();
+    println!("Listening on {}", address);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_connection(stream);
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    loop {
+        let mut buffer = [0; 1024];
+        match stream.read(&mut buffer) {
+            Ok(0) => {
+                // End of file, break the loop.
+                break;
+            }
+            Ok(n) => {
+                let message = String::from_utf8_lossy(&buffer[..n]);
+                println!("Received: {}", message);
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
---
+++ server/src/main.rs
@@ -0,0 +1,49 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use log::{info, warn};
+use std::collections::HashMap;
+use std::io::Write;
+use std::net::TcpStream;
+use std::sync::{Arc, Mutex};
+
+#[get("/")]
+async fn get_handler(socket: web::Data<Arc<Mutex<TcpStream>>>, info: web::Query<HashMap<String, String>>) -> impl Responder {
+    let query = serde_urlencoded::to_string(&*info).unwrap();
+    let mut socket = socket.lock().unwrap();
+    match socket.write(query.as_bytes()) {
+        Ok(_) => info!("Sent: {}", query),
+        Err(e) => warn!("Error sending data: {}", e),
+    };
+
+    HttpResponse::Ok().body("Received GET request")
+}
+
+#[post("/")]
+async fn post_handler(socket: web::Data<Arc<Mutex<TcpStream>>>, body: String) -> impl Responder {
+    let mut socket = socket.lock().unwrap();
+    match socket.write(body.as_bytes()) {
+        Ok(_) => info!("Sent: {}", body),
+        Err(e) => warn!("Error sending data: {}", e),
+    };
+
+    HttpResponse::Ok().body(format!("Received POST data: {}", body))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    let address = "127.0.0.1:7878";
+    let socket = Arc::new(Mutex::new(TcpStream::connect(address).unwrap()));
+    info!("Connected to {}", address);
+
+    let http_address = "192.168.50.88:1111";
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(Arc::clone(&socket)))
+            .service(get_handler)
+            .service(post_handler)
+    })
+    .bind(http_address)?
+    .run()
+    .await
+}
